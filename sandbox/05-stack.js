67edeec6f7f1d8fee51a308cfb9d85e1cdbfcc4b
volosgoto <volosgoto@gmail.com>
2021-02-09 10:34:29 +0200

    06

--- sandbox/05-stack.js
+++
@@ -1,9 +0,0 @@
-// // JavaScript Event Loop
-
-// setTimeout(() => { console.log('From setTimeot') }, 0);
-// console.log('Hello')
-// console.log('Start')
-// setTimeout(() => { console.log('From setTimeot 5000') }, 5000);
-
-// console.log('End')
-// setTimeout(() => { console.log('From setTimeot 2000') }, 2000);
