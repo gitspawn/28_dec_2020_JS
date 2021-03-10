5c30ade129cbb91580a1cc8ec51d6100a450586e
volosgoto <volosgoto@yahoo.com>
2021-03-10 21:46:18 +0200

    add ajax

---
+++ ajax-vanila-js/app.js
@@ -0,0 +1,38 @@
+// let btn = document.querySelector('button');
+// let output = document.querySelector('#output');
+// btn.addEventListener('click', addData);
+
+
+
+// function addData() {
+//   let xhr = new XMLHttpRequest();
+//   console.log(xhr);
+
+//   xhr.open("GET", "text.txt", true);
+
+//   xhr.onload = function () {
+//     console.log(this.readyState)
+
+//     if (this.status === 200) {
+//       output.innerHTML = this.responseText;
+//     }
+//   }
+
+//   xhr.send()
+// }
+
+
+
+// 
+// let xhr = new XMLHttpRequest();
+
+// xhr.onload = function () {
+//   if (xhr.status === 200) {
+//     console.log(JSON.parse(xhr.response))
+//   } else {
+//     console.log('Error!')
+//   }
+// }
+
+// xhr.open('GET', 'https://jsonplaceholder.typicode.com/users')
+// xhr.send();
