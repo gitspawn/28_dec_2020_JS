d7167bef878736b7e6800dee222ed8120958c132
volosgoto <volosgoto@gmail.com>
2021-04-19 17:26:58 +0300

    add lesson 6

--- 03-lesson. Events, State/src/components/App.js
+++ 03-lesson. Events, State/src/components/App.js
@@ -1,57 +1,56 @@
-import React, { Component } from 'react';
-import Layout from './Layout'
+import React, { Component } from "react";
+import Layout from "./Layout";
 // import Counter from './Counter'
-import TaskList from './TaskList/'
-import TaskEditor from './TaskEditor/'
-import createTask from '../utils/create-task'
-
-
+import TaskList from "./TaskList";
+import TaskEditor from "./TaskEditor";
+import createTask from "../utils/create-task";
 
 class App extends Component {
-  static defaulProps = {}
-  static propTypes = {}
+  static defaulProps = {};
+  static propTypes = {};
 
   state = {
-    tasks: []
-  }
+    tasks: [],
+  };
 
   addTask = () => {
     let task = createTask();
     // console.log(task)
     this.setState((prevState) => {
       return {
-        tasks: [...prevState.tasks, task]
-      }
-    })
-  }
+        tasks: [...prevState.tasks, task],
+      };
+    });
+  };
 
   removeTask = (taskId) => {
     // console.log('Remove task', taskId)
     this.setState((prevState) => {
       return {
         // tasks: prevState.tasks.filter((task) => { return task.id !== taskId })
-        tasks: prevState.tasks.filter(({ id }) => id !== taskId)
-      }
-    })
-  }
-
-
+        tasks: prevState.tasks.filter(({ id }) => id !== taskId),
+      };
+    });
+  };
 
   render() {
-    let { tasks } = this.state
+    let { tasks } = this.state;
     return (
       <Layout>
         {/* <Counter initialValue={0} step={5} /> */}
         {/* <TaskList tasks={this.state.tasks} /> */}
         <TaskEditor onAddTask={this.addTask} />
-        {tasks.length > 0 && <TaskList tasks={tasks} onremoveTask={this.removeTask} />}
+        {tasks.length > 0 && (
+          <TaskList tasks={tasks} onremoveTask={this.removeTask} />
+        )}
       </Layout>
-    )
+    );
   }
 }
 
+{
+  /* <Counter />
+new Counter */
+}
 
-{/* <Counter />
-new Counter */}
-
-export default App
+export default App;
