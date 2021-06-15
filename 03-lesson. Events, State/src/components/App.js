d29647b3b7f4d2530f0a13c889f7ad580c32f1ba
gitspawn <volosgoto@gmail.com>
2021-06-15 11:04:54 +0300

    upd lesson 3

--- 03-lesson. Events, State/src/components/App.js
+++ 03-lesson. Events, State/src/components/App.js
@@ -5,52 +5,58 @@ import TaskList from "./TaskList";
 import TaskEditor from "./TaskEditor";
 import createTask from "../utils/create-task";
 
-class App extends Component {
-  static defaulProps = {};
-  static propTypes = {};
-
-  state = {
-    tasks: [],
-  };
-
-  addTask = () => {
-    let task = createTask();
-    // console.log(task)
-    this.setState((prevState) => {
-      return {
-        tasks: [...prevState.tasks, task],
-      };
-    });
-  };
-
-  removeTask = (taskId) => {
-    // console.log('Remove task', taskId)
-    this.setState((prevState) => {
-      return {
-        // tasks: prevState.tasks.filter((task) => { return task.id !== taskId })
-        tasks: prevState.tasks.filter(({ id }) => id !== taskId),
-      };
-    });
-  };
-
-  render() {
-    let { tasks } = this.state;
-    return (
-      <Layout>
-        {/* <Counter initialValue={0} step={5} /> */}
-        {/* <TaskList tasks={this.state.tasks} /> */}
-        <TaskEditor onAddTask={this.addTask} />
-        {tasks.length > 0 && (
-          <TaskList tasks={tasks} onremoveTask={this.removeTask} />
-        )}
-      </Layout>
-    );
-  }
-}
+// function App() {
+//     <Layout>
+//         <Counter initialValue={0} step={1} />
+//     </Layout>;
+// }
 
 {
-  /* <Counter />
+    /* <Counter />
 new Counter */
+}
+
+class App extends Component {
+    static defaulProps = {};
+    static propTypes = {};
+
+    state = {
+        tasks: [],
+    };
+
+    addTask = () => {
+        let task = createTask();
+        // console.log(task)
+        this.setState((prevState) => {
+            return {
+                tasks: [...prevState.tasks, task],
+            };
+        });
+    };
+
+    removeTask = (taskId) => {
+        // console.log('Remove task', taskId)
+        this.setState((prevState) => {
+            return {
+                // tasks: prevState.tasks.filter((task) => { return task.id !== taskId })
+                tasks: prevState.tasks.filter(({ id }) => id !== taskId),
+            };
+        });
+    };
+
+    render() {
+        let { tasks } = this.state;
+        return (
+            <Layout>
+                {/* <Counter initialValue={0} step={5} /> */}
+                {/* <TaskList tasks={this.state.tasks} /> */}
+                <TaskEditor onAddTask={this.addTask} />
+                {tasks.length > 0 && (
+                    <TaskList tasks={tasks} onremoveTask={this.removeTask} />
+                )}
+            </Layout>
+        );
+    }
 }
 
 export default App;
